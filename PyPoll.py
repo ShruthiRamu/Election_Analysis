c4b5b7672c0bd0680f9b98a726dd31c5cb6b734c
Mac User <macuser@MacintoshUsers-MacBook-2.local>
2020-02-21 18:55:58 -0800

    Updating PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -30,14 +30,52 @@ with open(file_to_save, "w") as txt_file:
     txt_file.write("Counties in the election\n-------------------------------------\n")
     txt_file.write("Arapahoe\nDenver\nJefferson")'''
 
+
 file_to_load=os.path.join("Resources", "election_results.csv")
 file_to_save=os.path.join("analysis", "election_analysis")
+
+total_votes=0
+candidate_options=[]
+candidate_votes={}
+
+winning_candidate=""
+winning_count=0
+winning_percentage=0
 
 with open(file_to_load) as election_data:
     file_reader=csv.reader(election_data)
     headers=next(file_reader)
-    print(headers)
+    '''print(headers)'''
 
-    '''for row in file_reader:
-        print(row)'''
+    for row in file_reader:
+        total_votes+=1
+        candidate_name=row[2]
+        if(candidate_name not in candidate_options):
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name]=0
+        candidate_votes[candidate_name]+=1
+        #print(row)
 
+    for candidate in candidate_votes:
+        votes = candidate_votes[candidate]
+        vote_percentage = float(votes) / float(total_votes) * 100
+        print(f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n") 
+
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_percentage= vote_percentage
+            winning_candidate=candidate
+
+
+#print(total_votes)
+#print(candidate_options)
+#print(candidate_votes)
+winning_candidate_summary= (
+    f"------------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning vote count: {winning_count:,}\n"
+    f"Winning percentage: {winning_percentage:.1f}%\n"
+    f"------------------------------\n"
+)
+
+print(winning_candidate_summary)
