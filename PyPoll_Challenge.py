75dde463223cf96ea17ad001edb024fd7734c2f4
Mac User <macuser@MacintoshUsers-MacBook-2.local>
2020-02-21 20:04:03 -0800

    Adding PyPoll_Challenge.py

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -32,15 +32,24 @@ with open(file_to_save, "w") as txt_file:
 
 
 file_to_load=os.path.join("Resources", "election_results.csv")
-file_to_save=os.path.join("analysis", "election_analysis")
+file_to_save=os.path.join("analysis", "election_results.txt")
 
 total_votes=0
+total=0
 candidate_options=[]
 candidate_votes={}
+
+county_options=[]
+county_votes={}
 
 winning_candidate=""
 winning_count=0
 winning_percentage=0
+
+county_winning_name=""
+county_winning_count=0
+county_winning_percentage=0
+
 
 with open(file_to_load) as election_data:
     file_reader=csv.reader(election_data)
@@ -50,10 +59,16 @@ with open(file_to_load) as election_data:
     for row in file_reader:
         total_votes+=1
         candidate_name=row[2]
+        county_name=row[1]
         if(candidate_name not in candidate_options):
             candidate_options.append(candidate_name)
             candidate_votes[candidate_name]=0
         candidate_votes[candidate_name]+=1
+
+        if(county_name not in county_options):
+            county_options.append(county_name)
+            county_votes[county_name]=0
+        county_votes[county_name]+=1
         #print(row)
     
 with open(file_to_save, "w") as txt_file:
@@ -67,6 +82,31 @@ with open(file_to_save, "w") as txt_file:
 
     txt_file.write(total_vote_summary)
 
+    county_vote_summary =  (f"County Votes:\n")   
+    print(county_vote_summary)
+    txt_file.write(county_vote_summary)
+
+    for county in county_votes:
+        county_vote_count = county_votes[county]
+        count_percenatge = float(county_vote_count)/float(total_votes) * 100
+        county_summary = f"{county}: {count_percenatge:.1f}% ({county_vote_count:,})\n"
+        print(f"{county}: {count_percenatge:.1f}% ({county_vote_count:,})\n") 
+        txt_file.write(county_summary)
+
+        if(county_vote_count > county_winning_count and count_percenatge > county_winning_percentage):
+            county_winning_count = county_vote_count
+            county_winning_percentage=county_winning_percentage
+            county_winning_name = county
+    
+    county_max_summary = (
+        f"---------------------------\n"
+        f"Largest County Turnout: {county_winning_name}\n"
+        f"---------------------------\n"
+    )
+
+    txt_file.write(county_max_summary)
+    print(county_max_summary)
+
     for candidate in candidate_votes:
         votes = candidate_votes[candidate]
         vote_percentage = float(votes) / float(total_votes) * 100
@@ -79,10 +119,6 @@ with open(file_to_save, "w") as txt_file:
             winning_percentage= vote_percentage
             winning_candidate=candidate
 
-
-#print(total_votes)
-#print(candidate_options)
-#print(candidate_votes)
     winning_candidate_summary= (
         f"------------------------------\n"
         f"Winner: {winning_candidate}\n"
